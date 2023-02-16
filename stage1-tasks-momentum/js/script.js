6f4884c8a7e9686829d9bd8d90a304f5c64a248e
irinagrishkevich <bonivomal1996@gmail.com>
2023-02-16 15:00:07 +0100

    add currentDate

--- stage1-tasks-momentum/js/script.js
+++ stage1-tasks-momentum/js/script.js
@@ -1,1 +1,31 @@
-const time = document.querySelector('.time')
+const time = document.querySelector('time')
+const dateNow = document.querySelector('date')
+
+function showTime() {
+  function showDate() {
+    const date = new Date()
+    const options = {
+      month: 'long',
+      day: 'numeric',
+      timeZone: 'UTC',
+    }
+    const dayOfWeek = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ]
+    const dayName = dayOfWeek[date.getDay()]
+    const currentDate = date.toLocaleDateString('en-RU', options)
+    dateNow.textContent = dayName + ', ' + currentDate
+  }
+  showDate()
+  const date = new Date()
+  const currentTime = date.toLocaleTimeString()
+  time.textContent = currentTime
+  setTimeout(showTime, 1000)
+}
+showTime()
