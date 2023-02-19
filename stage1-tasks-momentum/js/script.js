7b6cd162eff62363f2976255fef94d9a5c125189
irinagrishkevich <bonivomal1996@gmail.com>
2023-02-19 16:25:17 +0100

    add slider picture

--- stage1-tasks-momentum/js/script.js
+++ stage1-tasks-momentum/js/script.js
@@ -1,6 +1,12 @@
 const time = document.querySelector('time')
 const dateNow = document.querySelector('date')
 const greeting = document.querySelector('.greeting')
+const body = document.querySelector('body')
+let randomNumber = Math.floor(Math.random() * 20) + 1
+let slideNext = document.querySelector('.slide-next')
+let slidePrev = document.querySelector('.slide-prev')
+console.log(slideNext)
+getRandomNum()
 
 function showTime() {
   showDate()
@@ -11,10 +17,12 @@ function showTime() {
   setTimeout(showTime, 1000)
 }
 showTime()
+
 function showGreeting() {
   const date = new Date()
+  const timeDay = date.getHours()
   const timesOfDay = ['night', 'morning', 'afternoon', 'evening']
-  const timeOfDayNum = Math.floor(date.getHours() / 6)
+  const timeOfDayNum = Math.floor(timeDay / 6)
   const timeOfDay = timesOfDay[timeOfDayNum]
   greeting.innerHTML = `Good ${timeOfDay}`
 }
@@ -50,3 +58,51 @@ function getLocalStorage() {
   }
 }
 window.addEventListener('load', getLocalStorage)
+function getRandomNum() {
+  if (randomNumber < 10) {
+    randomNumber = '0' + randomNumber
+  }
+  setBg()
+}
+
+slidePrev.addEventListener('click', getSlidePrev)
+function getSlideNext() {
+  if (randomNumber !== 21) {
+    if (randomNumber === 20) {
+      randomNumber = 1
+    } else {
+      randomNumber++
+    }
+  }
+  if (randomNumber < 10) {
+    randomNumber = '0' + randomNumber
+  }
+  setBg()
+}
+slideNext.addEventListener('click', getSlideNext)
+function getSlidePrev() {
+  if (randomNumber !== 0) {
+    if (randomNumber === '01') {
+      randomNumber = 20
+    } else {
+      randomNumber--
+    }
+  }
+  if (randomNumber < 10) {
+    randomNumber = '0' + randomNumber
+  }
+  setBg()
+}
+function setBg() {
+  const img = new Image()
+  img.src =
+    'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/' +
+    randomNumber +
+    '.jpg'
+  img.onload = () => {
+    body.style.backgroundImage =
+      "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/" +
+      randomNumber +
+      ".jpg')"
+  }
+}
